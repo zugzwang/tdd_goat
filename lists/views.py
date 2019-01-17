121ae5f116eccbf6b6f904145bc9358bc3d95e5f
Francisco V <fvial@grupolvt.cl>
2019-01-17 16:33:37 -0300

    Created List model, and attribute Item.list

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
@@ -7,7 +7,8 @@ def home_page(request):
     return render(request, 'home.html')
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/only_one/')
 
 def view_list(request):
