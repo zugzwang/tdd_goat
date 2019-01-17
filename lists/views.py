fba1d23c68c546d1c3d50ffa108d586ac44ee148
Francisco V <fvial@grupolvt.cl>
2019-01-17 18:36:15 -0300

    unifying polytope and fractal

--- lists/views.py
+++ lists/views.py
@@ -9,8 +9,9 @@ def home_page(request):
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/only_one/')
+    return redirect(f'/lists/{list_.id}/')
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'items': items})
